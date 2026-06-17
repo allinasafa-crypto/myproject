deb536b75f14a235eed415e61bc5b705e423414e
allinasafa-crypto <allinasafa@gmail.com>
2026-06-17 22:33:42 +0500

    Fix ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(ww2_(*o*ix&g-l!g%v3---1o-=7=8fp^2uruy3jvl&7%1+@&g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+DEBUG = False
 
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
